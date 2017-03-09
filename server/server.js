4231546abcf96d0f117ebe7efa24f933c9f5705b
John Livingston <jmlivingston@gmail.com>
2017-03-08 17:45:01 -0800

    Test first pass at SEO

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var expressHandlebars = require('express-handlebars');
+var seo = require('./seo.json');
+
 var app = express();
+app.engine('handlebars', expressHandlebars({ layoutsDir: '', defaultLayout: 'index', extname: '.html' }));
+app.set('view engine', 'handlebars');
+
 var path = require('path');
-app.use(express.static(__dirname + '/dist'));
+
+function handleSeo(res, req) {
+    res.render('home', {
+        helpers: {
+            foo: function () { 
+                var path = req.path.replace('/', '');
+                return seo[path] ? seo[path].title : seo.default.title;
+            }
+        }
+    });
+}
+
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + "/dist/index.htm");
+    handleSeo(res, req);
+});
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function (req, res) {
+    handleSeo(res, req);
 });
 var port = process.env.PORT || 5200;
 app.listen(port, function () {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageThreeComponent } from './page-three/page-three.component';
+
+const routes: Routes = [
+  { path: 'page-one', component: PageOneComponent },
+  { path: 'page-two', component: PageTwoComponent },
+  { path: 'page-three', component: PageThreeComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
